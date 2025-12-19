39d7d2013fb9c22979ccbd0e9a97dc3d20e196ba
Jun <june029882@yonsei.ac.kr>
2025-12-20 04:01:01 +0900

    router bug fixed; source and target cells costs applied correctly

--- router.c
+++ router.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#define MAX_COST 2147483647
+#include <string.h>
+#define MAX_INT 2147483647
+#define epsilon 1e-6
 typedef struct _cell{
     int cell_cost;
     int pred;
     int path_cost;
+    double score; // path_cost or estimated path cost
     int length; // length from source to this cell. used for backtracking
     int layer;
     int x;
@@ -16,18 +19,24 @@ typedef struct _net{
     int source_layer;
     int source_x;
     int source_y;
+    int source_cell_cost; // to block other nets from using the source cell
     int target_layer;
     int target_x;
     int target_y;
+    int target_cell_cost; // to block other nets from using the target cell
 } net;
 typedef struct _heap{
     int size;
     cell* arr;
 } heap;
 
+inline int abs (int a) {
+    return (a < 0) ? -a : a;
+}
+
 void heap_push(heap* hp, cell c) {
     int finding_pos = ++hp->size;
-    while (finding_pos != 1 && hp->arr[finding_pos >> 1].path_cost > c.path_cost) {
+    while (finding_pos != 1 && hp->arr[finding_pos >> 1].score > c.score) {
         hp->arr[finding_pos] = hp->arr[finding_pos >> 1];
         finding_pos >>= 1;
     }
@@ -39,8 +48,8 @@ cell heap_pop(heap* hp) {
     cell last = hp->arr[hp->size--];
     int child = 2;
     while (child <= hp->size) {
-        if (child < hp->size && hp->arr[child].path_cost > hp->arr[child + 1].path_cost) child++;
-        if (last.path_cost < hp->arr[child].path_cost) break;
+        if (child < hp->size && hp->arr[child].score > hp->arr[child + 1].score) child++;
+        if (last.score < hp->arr[child].score) break;
         hp->arr[child >> 1] = hp->arr[child];
         child <<= 1;
     }
@@ -48,25 +57,39 @@ cell heap_pop(heap* hp) {
     return ret;
 }
 
+int cost_predictor(cell current, cell target, int bend_cost, int via_cost) {
+    int x_diff = abs(current.x - target.x);
+    int y_diff = abs(current.y - target.y);
+    int layer_diff = abs(current.layer - target.layer);
+    int est_cost = x_diff + y_diff + layer_diff * via_cost;
+    if (layer_diff == 0 && x_diff != 0 && y_diff != 0) est_cost += bend_cost;
+    return est_cost;
+}
+
 net* nets;
 cell*** map;
+
 // UP, NORTH, EAST, WEST, SOUTH, DOWN
 int dx[6] = {0, 0, 1, -1, 0, 0};
 int dy[6] = {0, 1, 0, 0, -1, 0};
 int dz[6] = {1, 0, 0, 0, 0, -1};
 
 int main(int argc, char** argv) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: ./router <grid file> <output file>\n");
+    if (argc != 3 && argc != 4) {
+        fprintf(stderr, "Usage: ./router <grid file> <output file> or ./router <grid file> <output file> --p\n");
         return -1;
     }
-    int x_size, y_size; // z_size is fixed to 2
+    bool use_predictor = false;
+    if (argc == 4 && strcmp(argv[3], "--p") == 0) use_predictor = true; // will be implemented later
+    
     int bend_cost, via_cost;
     FILE* inputFile1 = fopen(argv[1], "r");
     if (inputFile1 == NULL) {
         fprintf(stderr, "Error opening file: %s\n", argv[1]);
         return -1;
     }
+
+    int x_size, y_size; // z_size is fixed to 2
     fscanf(inputFile1, "%d %d %d %d", &x_size, &y_size, &bend_cost, &via_cost);
     map = (cell***)malloc(3 * sizeof(cell**)); // layer number is not zero-indexed
     for (int z = 1; z <= 2; z++) {
@@ -93,6 +116,8 @@ int main(int argc, char** argv) {
     for (int i = 1; i <= net_count; i++) {
         fscanf(inputFile2, "%*d %d %d %d %d %d %d", &nets[i].source_layer, &nets[i].source_x, &nets[i].source_y,
                &nets[i].target_layer, &nets[i].target_x, &nets[i].target_y);
+        nets[i].source_cell_cost = map[nets[i].source_layer][nets[i].source_y][nets[i].source_x].cell_cost;
+        nets[i].target_cell_cost = map[nets[i].target_layer][nets[i].target_y][nets[i].target_x].cell_cost;
         map[nets[i].source_layer][nets[i].source_y][nets[i].source_x].cell_cost = -1; // to block other nets from using the source cell
         map[nets[i].target_layer][nets[i].target_y][nets[i].target_x].cell_cost = -1; // to block other nets from using the target cell
         map[nets[i].source_layer][nets[i].source_y][nets[i].source_x].length = 1;
@@ -101,8 +126,9 @@ int main(int argc, char** argv) {
 
     FILE* outputFile = fopen("output.txt", "w");
     heap hp;
-    hp.size = 0;
     hp.arr = (cell*)malloc((x_size * y_size * 2 + 1) * sizeof(cell));
+    cell* reversed_path = (cell*)malloc((x_size * y_size * 2) * sizeof(cell)); // worst case length
+
     // Routing using Dijkstra's algorithm
     for (int n = 1; n <= net_count; n++) {
         // reset map
@@ -110,45 +136,42 @@ int main(int argc, char** argv) {
             for (int y = 0; y < y_size; y++) {
                 for (int x = 0; x < x_size; x++) {
                     map[z][y][x].pred = -1;
-                    map[z][y][x].path_cost = MAX_COST;
+                    map[z][y][x].path_cost = MAX_INT;
+                    map[z][y][x].score = MAX_INT;
                     map[z][y][x].checked = false;
                 }
             }
         }
+        hp.size = 0;
         bool reached = false;
         
-        map[nets[n].source_layer][nets[n].source_y][nets[n].source_x].cell_cost = 1; // ensure starting cell is not an obstacle
-        map[nets[n].target_layer][nets[n].target_y][nets[n].target_x].cell_cost = 1; // ensure target cell is not an obstacle
-        map[nets[n].source_layer][nets[n].source_y][nets[n].source_x].path_cost = 1;
+        map[nets[n].source_layer][nets[n].source_y][nets[n].source_x].cell_cost = nets[n].source_cell_cost; // ensure starting cell is not an obstacle
+        map[nets[n].target_layer][nets[n].target_y][nets[n].target_x].cell_cost = nets[n].target_cell_cost; // ensure target cell is not an obstacle
+        map[nets[n].source_layer][nets[n].source_y][nets[n].source_x].path_cost = nets[n].source_cell_cost;
+        map[nets[n].source_layer][nets[n].source_y][nets[n].source_x].score = nets[n].source_cell_cost;
         map[nets[n].source_layer][nets[n].source_y][nets[n].source_x].checked = true;
         heap_push(&hp, map[nets[n].source_layer][nets[n].source_y][nets[n].source_x]);
+        int length;
+
         while(hp.size) {
+
             cell current = heap_pop(&hp);
+            map[current.layer][current.y][current.x].checked = true;
+
             if (current.layer == nets[n].target_layer && current.x == nets[n].target_x && current.y == nets[n].target_y) { // reached target
                 reached = true;
-                cell* reversed_path = (cell*)malloc(current.length * sizeof(cell));
 
-                // do backtracking and cleanup
+                // do backtracking
                 cell tracking = current;
                 while (tracking.layer != nets[n].source_layer || tracking.x != nets[n].source_x || tracking.y != nets[n].source_y) {
-                    map[tracking.layer][tracking.y][tracking.x].cell_cost = -1; // mark as obstacle
-                    reversed_path[map[tracking.layer][tracking.y][tracking.x].length - 1] = map[tracking.layer][tracking.y][tracking.x];
-                    int pd = map[tracking.layer][tracking.y][tracking.x].pred;
-                    tracking = map[tracking.layer + dz[pd]][tracking.y + dy[pd]][tracking.x + dx[pd]];
+                    reversed_path[tracking.length - 1] = tracking;
+                    tracking = map[tracking.layer + dz[tracking.pred]][tracking.y + dy[tracking.pred]][tracking.x + dx[tracking.pred]];
                 }
-                map[tracking.layer][tracking.y][tracking.x].cell_cost = -1; // mark as obstacle
                 reversed_path[0] = map[tracking.layer][tracking.y][tracking.x];
+                length = current.length;
                 
-                fprintf(outputFile, "%d\n", n);
-                for (int i = 0; i < current.length; i++) {
-                    fprintf(outputFile, "%d %d %d\n", reversed_path[i].layer, reversed_path[i].x, reversed_path[i].y);
-                    if (i != current.length - 1 && reversed_path[i].layer != reversed_path[i + 1].layer) { // via
-                        fprintf(outputFile, "3 %d %d\n", reversed_path[i].x, reversed_path[i].y);
-                    }
-                }
-                free(reversed_path);
                 // debugging
-                // printf("Cost: %d\n", current.path_cost);
+                printf("Cost: %d\n", current.path_cost);
                 break;
             }
             for (int dir = 0; dir < 6; dir++) {
@@ -158,24 +181,36 @@ int main(int argc, char** argv) {
                 if (nx < 0 || nx >= x_size || ny < 0 || ny >= y_size || nl < 1 || nl >= 3) continue;
                 if (map[nl][ny][nx].cell_cost == -1) continue; // obstacle
                 if (map[nl][ny][nx].checked) continue; // already checked
-
-                map[nl][ny][nx].checked = true;
+                
                 map[nl][ny][nx].path_cost = map[current.layer][current.y][current.x].path_cost + map[nl][ny][nx].cell_cost;
                 map[nl][ny][nx].length = map[current.layer][current.y][current.x].length + 1;
                 map[nl][ny][nx].pred = 5 - dir;
                 if (dir == 0 || dir == 5) map[nl][ny][nx].path_cost += via_cost;
-                else if (map[current.layer][current.y][current.x].pred != 0 && map[current.layer][current.y][current.x].pred != 5 && dir != 5 - map[current.layer][current.y][current.x].pred) map[nl][ny][nx].path_cost += bend_cost; // bend cost
+                else if (map[current.layer][current.y][current.x].pred != 0 && map[current.layer][current.y][current.x].pred != 5 && // not via
+                map[current.layer][current.y][current.x].pred != -1 && // current is not source
+                dir != 5 - map[current.layer][current.y][current.x].pred) // direction changed
+                    map[nl][ny][nx].path_cost += bend_cost;
+                map[nl][ny][nx].score = map[nl][ny][nx].path_cost;
                 heap_push(&hp, map[nl][ny][nx]);
             }
         }
-        if (reached == false) {
-            // printf("%d failed\n", n);
-            fprintf(outputFile, "%d\n", n);
+        fprintf(outputFile, "%d\n", n);
+        if (reached == true) {
+            for (int i = 0; i < length; i++) {
+                fprintf(outputFile, "%d %d %d\n", reversed_path[i].layer, reversed_path[i].x, reversed_path[i].y);
+                printf("%d %d %d: cost %d\n", reversed_path[i].layer, reversed_path[i].x, reversed_path[i].y, reversed_path[i].path_cost);
+                if (i != length - 1 && reversed_path[i].layer != reversed_path[i + 1].layer) { // via
+                    fprintf(outputFile, "3 %d %d\n", reversed_path[i].x, reversed_path[i].y);
+                }
+                // do cleanup
+                map[reversed_path[i].layer][reversed_path[i].y][reversed_path[i].x].cell_cost = -1; // block this cell for other nets
+            }
         }
         fprintf(outputFile, "0\n"); // end of net
     }
-    
+    fclose(outputFile);
     free(hp.arr);
+    free(reversed_path);
     for (int z = 1; z <= 2; z++) {
         for (int y = 0; y < y_size; y++) free(map[z][y]);
         free(map[z]);
